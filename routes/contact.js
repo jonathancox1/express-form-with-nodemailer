f40315925f26cada2289299babf7c28e63580393
Jonathan Cox <cox.jonathan@gmail.com>
2020-05-19 13:22:25 -0400

    form submission to nodemailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,21 @@
 const express = require('express');
 // create router
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+// loads contents into process.env
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+    },
+})
 
 
 // GET /contact
@@ -13,13 +28,22 @@ router.get('/', (req, res) => {
 
 // POST /contact
 router.post('/', (req, res) => {
-    console.log(req.body);
-
-    res.render('contact', {
-        title: 'Thank you',
-        submitted: true,
+    transporter.sendMail({
+        from: process.env.MAIL_FROM, // sender address
+        to: process.env.MAIL_TO, // list of receivers
+        subject: `New Contact From ${req.body.name}`,
+        html: `<b>New Contact</b>
+        <b>Email: </b>${req.body.email}
+        <b>Name: </b>${req.body.name}
+        <b>Comments: </b>${req.body.comments}`,
     })
+        .then(status => {
+            console.log(status);
+
+            res.render('contact', {
+                title: 'Thank you',
+                submitted: true,
+            })
+        })
 })
-
-
 module.exports = router;
